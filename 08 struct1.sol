6a7055b75fb7b61b6b64d87fd3723201b40fd912
Leon <leondo123@gmail.com>
2017-06-15 20:48:37 -0400

    struts are weird

---
+++ 08 struct1.sol
@@ -0,0 +1,26 @@
+/*
+    similar to
+    
+    var mySpecialVar = {string1: 'hello', string2: 'world'}
+*/
+
+pragma solidity ^0.4.0;
+
+contract myContract {
+
+    struct MyStruct{
+        string string1;
+        string string2;
+    }
+
+    MyStruct public mySpecialVar;
+    
+    function myFunction() returns(string){
+        mySpecialVar = MyStruct('hello', 'world');
+        return mySpecialVar.string1;
+    }
+    
+}
+
+
+
